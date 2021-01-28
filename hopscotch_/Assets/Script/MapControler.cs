adf07d1901f1879fca7b645265d48d426a755c37
CHIHYUNMOON <77661256+CHIHYUNMOON@users.noreply.github.com>
2021-01-28 17:41:19 +0900

    2021.01.28

--- hopscotch_/Assets/Script/MapControler.cs
+++ hopscotch_/Assets/Script/MapControler.cs
@@ -4,18 +4,42 @@ using UnityEngine;
 
 public class MapControler : MonoBehaviour
 {
-    Tile tile;
-
+    
+    
     public GameObject _tile;
+
+    public void LocateTile(int level)     
+    {
+        int sidelength = level + 4;
+        
+        for (int i = 0; i <  2 * sidelength-1; i++)
+        {
+            for (int j = 0; j < sidelength+i; j++)
+            {
+                if (j <i)
+                {
+                    GameObject maptile = Instantiate(this._tile) as GameObject;
+                    maptile.transform.position = new Vector3((float)0.5 * i - j, 0.0f, (float)0.866 * i);
+
+                }
+                else
+                {
+                    GameObject maptile = Instantiate(this._tile) as GameObject;
+                    maptile.transform.position = new Vector3((float)0.5 * i + j, 0.0f, (float)0.866 * i);
+                    
+                }
+            } 
+        }
+    }
     // Start is called before the first frame update
     void Start()
     {
-        
+        LocateTile(1);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+   
     }
 }
--- hopscotch_/Assets/Script/Tile.cs
+++ hopscotch_/Assets/Script/Tile.cs
@@ -1,14 +1,23 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Tile : MonoBehaviour
 {
+    public int _score;
+
+    Tile(Vector3 Tilelocation) {
+        _score = UnityEngine.Random.Range(1, 6);
+        transform.position = Tilelocation;
+
+
+    }
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        _score = UnityEngine.Random.Range(1, 5);
     }
 
     // Update is called once per frame
