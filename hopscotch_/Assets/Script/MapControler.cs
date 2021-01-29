3bd2d984fc2ec332fb190fa57a39e3df4c4da07c
CHIHYUNMOON <77661256+CHIHYUNMOON@users.noreply.github.com>
2021-01-29 17:40:02 +0900

    2021.01.29 학원

--- hopscotch_/Assets/Script/MapControler.cs
+++ hopscotch_/Assets/Script/MapControler.cs
@@ -10,16 +10,22 @@ public class MapControler : MonoBehaviour
     
     public void CreateMapArr(int level) 
     {
+        
         int sidelength = level + 4;
-        for (int i = 0; i < 2 * sidelength - 1; i++) {
+        int height = 2 * sidelength - 1;
+        MapArr = new Vector3[height][];
+        for (int i = 0; i < height; i++) {
+            
             for (int j = 0; j < sidelength +i; j++) {
-                if (i < sidelength)
+                if (i <= sidelength)
                 {
+                    MapArr[i] = new Vector3[sidelength + i];
                     MapArr[i][j] = new Vector3(-0.5f * i + j, 0.0f, 0.866f * j);
                 }
                 else
                 {
-                    MapArr[i][j] = MapArr[2 * sidelength - 2 - i][j];
+                    MapArr[i] = new Vector3[MapArr[height-1].Length];
+                    MapArr[i][j] = MapArr[2 * sidelength -i][j];
                 }
             }
         }
@@ -28,9 +34,9 @@ public class MapControler : MonoBehaviour
     }
     public void LocateTile(Vector3[][] mapArr)     
     {
-        for (int i = 0; i < mapArr.GetLength(0); i++)
+        for (int i = 0; i < mapArr.Length; i++)
         {
-            for (int j = 0; j < mapArr.GetLength(1) ; j++) {
+            for (int j = 0; j < mapArr[i].Length ; j++) {
                 GameObject maptile = Instantiate(this._tile);
                 maptile.transform.position = mapArr[i][j];
             }
